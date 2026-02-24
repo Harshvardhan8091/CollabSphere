527b22b04eac2319592a58252349f500e508677a
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-02-24 10:35:13 +0530

    Auth Routes + Middleware

---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+module.exports.AuthController = require("./authController");
+module.exports.HealthController = require("./healthController");
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,18 @@
 
 const express = require("express");
 const { AuthController } = require("../controllers");
+const protect = require("../middleware/protect");
 
 const router = express.Router();
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
 router.post("/logout", AuthController.logout);
+
+// Protected route: returns current user (requires Bearer token)
+router.get("/me", protect, (req, res) => {
+  res.json(req.user);
+});
 
 module.exports = router;
 
